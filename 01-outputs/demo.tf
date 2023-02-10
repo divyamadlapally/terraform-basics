b0fddd0a4c669c08bf77d5a926aaaf92f15844ba
divyamadlapally <divyamadlapally@gmail.com>
2023-02-10 17:58:47 +0530

    auto push

--- 01-outputs/demo.tf
+++ 01-outputs/demo.tf
@@ -15,5 +15,15 @@ output "print-demo" {
 }
 
 # Note : A variable can be accessed without ${} as well , only if its the only declared entry in the part of the output.
-// If you're using the variable in between the strings, you need to enclose them in the quotes and ${}.
-// there is no concept of single quotses ' ' in terraform
+#// If you're using the variable in between the strings, you need to enclose them in the quotes and ${}.
+#// there is no concept of single quotses ' ' in terraform
+
+variable "example-list" {
+    default = [
+        "aws",
+        "DevOps",
+        100,
+        true
+
+    ]
+}
