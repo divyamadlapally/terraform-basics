2053d24a20d8441043c98ef5967c20929316aa12
divyamadlapally <divyamadlapally@gmail.com>
2023-02-10 19:55:08 +0530

    auto push

--- 03-ec2/ec2.tf
+++ 03-ec2/ec2.tf
@@ -1,3 +1,12 @@
 provider "aws" {
     region = "us-east-1"
 }
+# creates ec2 instance
+resource "aws_instance" "this" {
+  ami                     = "ami-0e3aeafe193bbdd4a"
+  instance_type           = "t2.micro"
+
+  tags = {
+    Name = "Terraform-Server"
+  }
+}
