fb422a4d7c8be8c588ae664a3e42e2f85ac3410f
divyamadlapally <divyamadlapally@gmail.com>
2023-02-10 17:39:21 +0530

    auto push

--- 01-outputs/demo.tf
+++ 01-outputs/demo.tf
@@ -1,3 +1,19 @@
 output "sample" {
   value = "Hello World"
 }
+
+variable "demo" {
+    value = "Terraform Training"
+}
+
+output "printing-demo" {
+    value = "value is ${var.demo}"
+}
+
+output "print-demo" {
+    value = var.demo
+}
+
+# Note : A variable can be accessed without ${} as well , only if its the only declared entry in the part of the output.
+// If you're using the variable in between the strings, you need to enclose them in the quotes and ${}.
+// there is no concept of single quotses ' ' in terraform
