367aa1b32217393133eb2be2cb282f80e12175ce
divyamadlapally <divyamadlapally@gmail.com>
2023-02-10 20:03:12 +0530

    auto push

--- 03-ec2/ec2.tf
+++ 03-ec2/ec2.tf
@@ -1,6 +1,7 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
+
 # creates ec2 instance
 resource "aws_instance" "this" {
   ami                     = "ami-0e3aeafe193bbdd4a"
