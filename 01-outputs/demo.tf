29a1bf65892f8707bb15155009b321b1fe6aaa2a
divyamadlapally <divyamadlapally@gmail.com>
2023-02-10 18:35:38 +0530

    auto push

--- 01-outputs/demo.tf
+++ 01-outputs/demo.tf
@@ -31,3 +31,17 @@ variable "example-list" {
  output "ex-list-op" {
     value = "Welcome to ${var.example-list[0]} and ${var.example-list[1]} Training, Training duration is ${var.example-list[2]} hours, we can also apply for ${var.example-list[4]}"
  }
+
+ #this is how we declare map variable
+
+ variable "example-map" {
+    default = {
+        Duration = 105,
+        Trainer = "verma",
+        cloud = "AWS"
+    }
+ }
+
+ output "ex-map-op" {
+    value = " Welcome to devops with ${var.example-map["cloud"]} Training and the training duration is ${var.example-map["Duration"]}, trainer name is ${var.example-map["Trainer"]}"
+ }
