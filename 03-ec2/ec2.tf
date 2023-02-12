ee53431ea7d7e1b6c07f5a9f3d25da363ccea870
divyamadlapally <divyamadlapally@gmail.com>
2023-02-12 08:12:53 +0530

    03-ec2

--- 03-ec2/ec2.tf
+++ 03-ec2/ec2.tf
@@ -6,6 +6,7 @@ provider "aws" {
 resource "aws_instance" "this" {
   ami                     = "ami-0e3aeafe193bbdd4a"
   instance_type           = "t2.micro"
+  vpc_security_group_ids  = [aws_security_group.allow_ssh.id]
 
   tags = {
     Name = "Terraform-Server"
