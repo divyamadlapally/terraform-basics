11fab6cdff87972645a27bddc85704e09609cf64
divyamadlapally <divyamadlapally@gmail.com>
2023-02-12 07:58:13 +0530

    03-ec2

--- 03-ec2/ec2.tf
+++ 03-ec2/ec2.tf
@@ -12,6 +12,34 @@ resource "aws_instance" "this" {
   }
 }
 
+# To create security group
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+
+  ingress {
+    description      = "SSH from SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+   
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
 # printing the public ip address of the machine
 output "print-public-dns" {
   value = aws_instance.this.public_dns
