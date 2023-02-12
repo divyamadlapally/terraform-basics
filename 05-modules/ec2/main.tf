fdd0b4b862f322102639333f18a9aa7bf5c3b98b
divyamadlapally <divyamadlapally@gmail.com>
2023-02-12 13:03:48 +0530

    auto-push

--- 05-modules/ec2/main.tf
+++ 05-modules/ec2/main.tf
@@ -1,10 +1,14 @@
 resource "aws_instance" "this" {
   ami                     = "ami-0e3aeafe193bbdd4a"
   instance_type           = "t2.micro"
-  vpc_security_group_ids  = []
+  vpc_security_group_ids  = [var.sg]
 
 }
 
 variable "sg" {
-    
+
+}
+
+output "public_ip" {
+    value = aws_instance.this.public_ip
 }
