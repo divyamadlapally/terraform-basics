bfe3da8b59c0e8fe37ade772ac78c52fc0a44c71
divyamadlapally <divyamadlapally@gmail.com>
2023-02-12 07:16:52 +0530

    03-ec2

--- 03-ec2/ec2.tf
+++ 03-ec2/ec2.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 # creates ec2 instance
-resource "aws_instance" "my-ec2" {
+resource "aws_instance" "this" {
   ami                     = "ami-0e3aeafe193bbdd4a"
   instance_type           = "t2.micro"
 
