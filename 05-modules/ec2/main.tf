6cf74a39185804f9200d185ee1c00ad0aafdf0d9
divyamadlapally <divyamadlapally@gmail.com>
2023-02-12 13:41:58 +0530

    auto-push

--- 05-modules/ec2/main.tf
+++ 05-modules/ec2/main.tf
@@ -1,14 +1,14 @@
-resource "aws_instance" "this" {
+# creates ec2 instance
+resource "aws_instance" "my-ec2" {
   ami                     = data.aws_ami.lab-image.image_id
   instance_type           = "t2.micro"
   vpc_security_group_ids  = [var.sg]
-
 }
 
-variable "sg" {
+# Before you use the variable from the root module, ensure you declare an empty variable.
+variable "sg" {}
 
-}
 
 output "public_ip_address" {
-    value = aws_instance.this.public_ip
+    value = aws_instance.my-ec2.public_ip
 }
