3689b722b74f840d6a2b67349620ea35530d67ee
divyamadlapally <divyamadlapally@gmail.com>
2023-02-14 17:07:42 +0530

    auto-push

--- 05-modules/ec2/data.tf
+++ 05-modules/ec2/data.tf
@@ -1,7 +1,7 @@
 data "aws_ami" "lab-image" {
   most_recent      = true
-  name_regex       = "ansible"
-  owners           = [907089808340]
+  name_regex       = "b52-ansible-dev-20Jan2023"
+  owners           = [355449129696]
 }
 
 
