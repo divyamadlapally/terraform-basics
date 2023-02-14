da9bbf6dcd1082cde037d06c1b166950d598bf3f
divyamadlapally <divyamadlapally@gmail.com>
2023-02-14 18:44:19 +0530

    auto-push

--- 05-modules/ec2/main.tf
+++ 05-modules/ec2/main.tf
@@ -3,6 +3,19 @@ resource "aws_instance" "my-ec2" {
   ami                     = data.aws_ami.lab-image.image_id
   instance_type           = "t2.micro"
   vpc_security_group_ids  = [var.sg]
+
+   provisioner "remote-exec" {
+    connection {
+    type     = "ssh"
+    user     = "centos"
+    password = "DevOps321"
+    host     = self.public_ip
+  }
+
+    inline = [
+      
+    ]
+  }
 }
 
 # Before you use the variable from the root module, ensure you declare an empty variable.
---
+++ 06-functions/count/main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# creates ec2 instance
+resource "aws_instance" "this" {
+  count                   = length(var.envs)
+  ami                     = "ami-0e3aeafe193bbdd4a"
+  instance_type           = "t2.micro"
+ 
+
+  tags = {
+    Name = "var.envs[count.index]"
+  }
+}
+
+output "public_ip" {
+   value = aws_instance.this.*public_ip
+}
+
+variable "envs" {
+    default = ["dev", "qa", "stage", "prod"]
+}
